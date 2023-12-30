5568a38de6054f1623acf4123e4faee43ee0378d
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-12-30 13:09:06 +0530

    modified 3 files in src

--- src/Users.jsx
+++ src/Users.jsx
@@ -11,6 +11,17 @@ function Users() {
       .then((result) => setUsers(result.data))
       .catch((err) => console.log(err));
   }, []);
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:3001/deleteUser/" + id)
+      .then((res) => {
+        console.log(res);
+        window.location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="d-flex vh-100 bg-dark justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
@@ -33,10 +44,15 @@ function Users() {
                 <td>{user.email}</td>
                 <td>{user.age}</td>
                 <td>
-                  <Link to="/update" className="btn btn-warning">
+                  <Link to={"/update/${user._id}"} className="btn btn-warning">
                     Update
                   </Link>
-                  <button className="btn btn-danger">Delete</button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={(e) => handleDelete(user._id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
